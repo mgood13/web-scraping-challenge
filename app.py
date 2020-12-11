c57fa089914f632e22ddd0f2a97dcc9ce24ff4f7
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2020-12-10 19:33:14 -0500

    Adding the html structure

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect
 from scrape_mars import scrape
-
-
+import pymongo
+from flask_pymongo import PyMongo
+#conn = 'mongodb://localhost:27017'
+#client = pymongo.MongoClient(conn)
+#db = client.marsDB
 
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def home():
-    greeting = 'hello'
-    return jsonify(greeting)
+    marsdata = mongo.db.mars_data.find_one()
+    return render_template('index.html', structure = marsdata)
 
 
 @app.route("/scrape")
 def scrape_html():
+    if "mars_data" in mongo.db.list_collection_names():
+        collection = mongo.db['mars_data']
+        collection.drop()
     html_structure = scrape()
-    print('here')
-    return render_template('index.html', structure = html_structure)
-
-
+    data = mongo.db.mars_data
+    data.insert_one(html_structure)
+    return redirect("/", code=302)
 
 
 if __name__ == '__main__':
